0683ad14f451763af76d50f14e04a5448751a6f4
Phinocio <github@phinocio.com>
2018-12-07 18:47:53 -0800

    Added jsdoc tags for the Team interface.

--- src/api/types/Team.ts
+++ src/api/types/Team.ts
@@ -5,9 +5,20 @@ import Venue from './Venue';
 
 /**
  * @typedef {object} Team
- * @property {string} abbreviation - The teams name in abbpreviated form.
+ * @property {string} abbreviation - The team's name in abbpreviated form.
  * @property {boolean} active - Indicates whether the team is currently active in the NHL.
- * @description A representation of an NHL Team.
+ * @property {Conference} conference - The conference data that the team belongs to.
+ * @property {Division} division - The division data that the team belongs to.
+ * @property {string} firstYearOfPlay - The first year the team played in the league.
+ * @property {number} id - The ID of the team in the API (1: New Jersey, 21: Colorado, etc).
+ * @property {string} locationName - The team's location name. EG: "Anaheim" for the Ducks.
+ * @property {string} name - The team's full name. EG: "Anaheim Ducks".
+ * @property {Array<Player>} roster - The team's roster, optional. Contianing an array of players on the team.
+ * @property {string} shortName - The team's short name. EG: "NY Islanders" for NYI.
+ * @property {string} siteUrl - The team's official website url.
+ * @property {string} teamName - The team's name. EG: "Ducks" for Anaheim.
+ * @property {Venue} venue - Data of the venue the team plays in.
+ * @description - A representation of a team.
  */
 interface Team {
   abbreviation: string;
