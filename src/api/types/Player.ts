2dc6750cb962d0e65fb626a3fec57e5d1bdcc967
Phinocio <github@phinocio.com>
2018-12-07 19:34:18 -0800

    Added jsdoc tags for Player interface

--- src/api/types/Player.ts
+++ src/api/types/Player.ts
@@ -1,7 +1,22 @@
 /**
  * @typedef {object} Player
- * @property {string} fullName The full name of the player.
- * @description A representation of an NHL Player.
+ * @property {number} age - A player's age.
+ * @property {Date} birthDate - A player's birth date.
+ * @property {string} birthPlace - Where a player was born.
+ * @property {string} dominantSide - The side in which a skater shoots, or a goalie catches.
+ * @property {string} firstName - A player's first name.
+ * @property {string} fullName - A player's full name.
+ * @property {string} height - How tall a player is.
+ * @property {string} id - The ID of the player returned by the NHL API.
+ * @property {boolean} isAlternateCaptain - Whether a player is an alternate captain or not.
+ * @property {boolean} isCaptain - Whether a player is a captain or not.
+ * @property {boolean} isRookie - Whether a player is a rookie or not.
+ * @property {string} jerseyNumber - The number a player wears on their jersey.
+ * @property {string} lastName - A player's last name.
+ * @property {string} position - The position a player plays.
+ * @property {string} rosterStatus - Whether a player is on the roster or not. (Y, N, I).
+ * @property {string} weight - How much a player weighs.
+ * @description - A representation of a PLayer.
  */
 interface Player {
   age: number;
